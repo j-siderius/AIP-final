ca5189a8b1adc7322a36620c74a4ef3ad91113c6
Jannick <nick.rius@gmail.com>
2021-12-16 16:23:40 +0100

    add player class, updated project description

---
+++ Player.py
@@ -0,0 +1,18 @@
+from Screen import Screen
+
+class Player:
+
+    def __init__(self):
+        pass
+
+    def draw_player(self, screen: Screen):
+        pass
+
+    def update_player(self):
+        pass
+
+    def player_input(self, keyPresses):
+        pass
+
+    def move_player(self):
+        pass

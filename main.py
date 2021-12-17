aa3be942772f3a73169e8fd59e8fbffd860f14a7
Jannick <nick.rius@gmail.com>
2021-12-17 21:04:56 +0100

    added Player init and display function calls

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from Screen import Screen
 from Field import Field
+from Player import Player
 import time
 
 
@@ -11,12 +12,14 @@ class program:
         self.screen = Screen(0, 0, self.loop, title="Test Terrain")
 
         self.field = Field(self.screen, field_pos=(0, 0), hex_width='auto', field_size=(self.screen.get_size()), hex_amount=(75, 0))
+        self.player = Player(self.screen, field_size=(self.screen.get_size()), field=self.field, hex_amount=(75, 0))
 
         self.screen.start()
 
     def loop(self):
         self.screen.background(139, 69, 19)
         self.field.display()
+        self.player.display()
 
 
 if __name__ == '__main__':

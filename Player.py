345c21b87629510b92c35b88a46c5d197a48d0e9
Jannick <nick.rius@gmail.com>
2021-12-18 22:14:46 +0100

    fixed player spawning and alignment to the board

--- Player.py
+++ Player.py
@@ -8,10 +8,10 @@ class Player:
 
         self.screen_size = field_size
 
+        self.x, self.y = None, None
+        self.radius = (field_size[0] / hex_amount[0]) / 2.3  # base this on hex_size of Tiles
         # assign starting position to a viable tile
         self.find_starting_tile(int(hex_amount[0]/2), int(hex_amount[1]/2))
-
-        self.radius = (field_size[0] / hex_amount[0]) / 2.3  # base this on hex_size of Tiles
 
         self.color = (255, 0, 0)
 
@@ -41,6 +41,7 @@ class Player:
             print(f"Starting tile found: ({tileX},{tileY})")
             print(tile.is_walkable())
             hex_size = self.field.get_hex_size()
-            # TODO: fix spacing / placement of player
-            self.x = (tileX * hex_size)
-            self.y = (tileY * hex_size)
+            # points are arranged in the following manner: [0]=bottom-right, [1]=bottom-left, [2]=middle-left,
+            # [3]=top-left, [4]=top-right, [5]=middle-right
+            self.x = self.field.get_tile(tileX, tileY).get_points()[4][0] - (self.field.get_hex_width() / 3)
+            self.y = self.field.get_tile(tileX, tileY).get_points()[2][1]

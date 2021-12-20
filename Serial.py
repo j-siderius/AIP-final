6ef7c12f3190b1184ed620b83ddaefeb3bcc1b11
Jannick <nick.rius@gmail.com>
2021-12-20 22:11:22 +0100

    added nonblocking serial listener added serial writer Arduino code is in this file as backup

--- Serial.py
+++ Serial.py
@@ -1,41 +1,63 @@
 import serial
+import Screen
 import time
 
 
 class Serial:
     def __init__(self, port, baud_rate):
         self.port = serial.Serial(port, baud_rate, timeout=0)
-        pass
+        self.ledBool = True
 
     def update(self):
-        if self.port.in_waiting > 0:
-            self.port.readline().decode('ascii')
-        pass
+        waiting = self.port.in_waiting
+        if waiting > 0:
+            msg = self.port.read(waiting).decode('ascii')
+            print(msg)
+
+        # test code
+        if 18 in Screen.Screen.get_pressed_keys(self):
+            print('O Pressed')
+            if self.ledBool:
+                self.send("on\n")
+                self.ledBool = False
+            else:
+                self.send("off\n")
+                self.ledBool = True
 
     def send(self, argument):
-        self.port.write(argument.encode())
+        arg = bytes(argument.encode())
+        self.port.write(arg)
+
 
 '''
-Arduino code:
-    String incomingBytes;
-    
-    void setup() {
-      Serial.begin(9600);
-      pinMode(LED_BUILTIN, OUTPUT);
+## Arduino code: ##
+String incomingBytes;
+unsigned long prevMillis = 0;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    incomingBytes = Serial.readStringUntil('\n');
+    if (incomingBytes == "on") {
+      Serial.write("LED ON");
+      digitalWrite(LED_BUILTIN, LOW);
+    } else if (incomingBytes == "off") {
+      Serial.write("LED OFF");
+      digitalWrite(LED_BUILTIN, HIGH);
+    } else {
+      Serial.write("invalid");
     }
-    
-    void loop() {
-      if (Serial.available() > 0) {
-        incomingBytes = Serial.readStringUntil('\n');
-        if (incomingBytes == "on") {
-          Serial.write("LED ON");
-          digitalWrite(LED_BUILTIN, LOW);
-        } else if (incomingBytes == "off") {
-          Serial.write("LED OFF");
-          digitalWrite(LED_BUILTIN, HIGH);
-        } else {
-          Serial.write("invalid");
-        }
-      }
-    }
+  }
+  if (digitalRead(D8)) {
+    Serial.write("BTN8 PRESSED");
+  }
+  if (millis() > prevMillis + 3000) {
+    Serial.write("TIME MESSAGE");
+    prevMillis = millis();
+  }
+}
 '''

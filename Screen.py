eb2ed1b8ef647d5e0e5fc201edde4bb66811a964
Jannick <nick.rius@gmail.com>
2021-12-20 21:40:34 +0100

    added keypress values to file

--- Screen.py
+++ Screen.py
@@ -153,6 +153,20 @@ class Screen:
     def get_mouse_pos(self):
         return pygame.mouse.get_pos()
 
+    '''
+    Keycodes:
+    1   30      q   20      a   4       z   29
+    2   31      w   26      s   22      x   27
+    3   32      e   8       d   7       c   6
+    4   33      r   21      f   9       v   25
+    5   34      t   23      g   10      b   5
+    6   35      y   28      h   11      n   17
+    7   36      u   24      j   13      m   16
+    8   37      i   12      k   14      ,   54
+    9   38      o   18      l   15      .   55
+    0   39      p   19      ;   51      /   56
+    -   45      +   46      '   52      
+    '''
     def get_pressed_keys(self):
         pressed_keys = pygame.key.get_pressed()
         active_keys = set(())

78bc5ff97769626ece278a6f43849189267449a3
Jannick <nick.rius@gmail.com>
2021-12-21 12:02:32 +0100

    added LED and nunchuck Arduino code

--- Serial.py
+++ Serial.py
@@ -61,3 +61,58 @@ void loop() {
   }
 }
 '''
+
+'''
+#include <WiiChuck.h>
+#include <FastLED.h>
+
+#define LED_PIN     D3
+#define NUM_LEDS    12
+#define BRIGHTNESS  25
+#define LED_TYPE    WS2811
+#define COLOR_ORDER GRB
+CRGB leds[NUM_LEDS];
+
+Accessory nunchuck1;
+
+unsigned long ledMillis, chuckMillis;
+
+void setup() {
+  Serial.begin(115200);
+
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(  BRIGHTNESS );
+
+  nunchuck1.begin();
+  if (nunchuck1.type == Unknown) {
+    nunchuck1.type = NUNCHUCK;
+  }
+
+  ledMillis = chuckMillis = 0;
+}
+
+void loop() {
+  unsigned long curMillis = millis();
+  if (curMillis > ledMillis + 10) {
+    ledMillis = curMillis;
+    leds[0] = CRGB::Red;
+    leds[1] = CRGB::Red;
+    leds[2] = CRGB::Red;
+    leds[3] = CRGB::Red;
+    leds[4] = CRGB::Black;
+    leds[5] = CRGB::Blue;
+    leds[6] = CRGB::Blue;
+    leds[7] = CRGB::Yellow;
+    leds[8] = CRGB::Blue;
+    leds[9] = CRGB::Blue;
+    leds[10] = CRGB::Blue;
+    leds[11] = CRGB::Black;
+    FastLED.show();
+  }
+  if (curMillis > chuckMillis + 250) {
+    chuckMillis = curMillis;
+    nunchuck1.readData();    // Read inputs and update maps
+    nunchuck1.printInputs(); // Print all inputs
+  }
+}
+'''

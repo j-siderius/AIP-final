f7f8c51fc52f625fd284664c9b5f76d712ca694e
Jannick <nick.rius@gmail.com>
2021-12-21 23:19:32 +0100

    added Arduino code for LED control and controller serial output added serial reader and conversion

--- Serial.py
+++ Serial.py
@@ -1,6 +1,6 @@
 import serial
 import Screen
-import time
+import math
 
 
 class Serial:
@@ -12,7 +12,14 @@ class Serial:
         waiting = self.port.in_waiting
         if waiting > 0:
             msg = self.port.read(waiting).decode('ascii')
-            print(msg)
+            if 'NUNCHUCK' in msg:
+                print(msg[msg.index('JoyX:'): msg.index('\r\n')])
+                # calculating angle values for selection:
+                # angle = math.degrees(math.atan2((JoyY-128), (JoyX-128))) + 180.0
+            elif 'STATUS' in msg:
+                print(msg)
+            else:
+                print('Serial error')
 
         # test code
         if 18 in Screen.Screen.get_pressed_keys(self):
@@ -31,38 +38,7 @@ class Serial:
 
 '''
 ## Arduino code: ##
-String incomingBytes;
-unsigned long prevMillis = 0;
 
-void setup() {
-  Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
-}
-
-void loop() {
-  if (Serial.available() > 0) {
-    incomingBytes = Serial.readStringUntil('\n');
-    if (incomingBytes == "on") {
-      Serial.write("LED ON");
-      digitalWrite(LED_BUILTIN, LOW);
-    } else if (incomingBytes == "off") {
-      Serial.write("LED OFF");
-      digitalWrite(LED_BUILTIN, HIGH);
-    } else {
-      Serial.write("invalid");
-    }
-  }
-  if (digitalRead(D8)) {
-    Serial.write("BTN8 PRESSED");
-  }
-  if (millis() > prevMillis + 3000) {
-    Serial.write("TIME MESSAGE");
-    prevMillis = millis();
-  }
-}
-'''
-
-'''
 #include <WiiChuck.h>
 #include <FastLED.h>
 
@@ -70,47 +46,84 @@ void loop() {
 #define NUM_LEDS    12
 #define BRIGHTNESS  25
 #define LED_TYPE    WS2811
-#define COLOR_ORDER GRB
+#define COLOR_ORDER RGB
 CRGB leds[NUM_LEDS];
+
+int Red = 0xFF0000;
+int Green = 0x00FF00;
+int Blue = 0x0000FF;
+int LightBlue = 0x00000D;
+int Yellow = 0xAFFF00;
+int White = 0xAAAAAA;
+int Black = 0x000000;
 
 Accessory nunchuck1;
 
-unsigned long ledMillis, chuckMillis;
+unsigned long ledMillis, controllerMillis;
+int sunMoonPhase = 0; //from 0 to 11, describes the sun and moon phase >> 0=beginning of day, 5=end of daytime, 6=beginning of night, 11=end of nighttime
+int healthTracker = 0; //full health=0, half health=2, dead=4
+int _controllerUpdateRate = 17; //Hz=1/(millis*1000)
 
 void setup() {
   Serial.begin(115200);
 
-  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
   FastLED.setBrightness(  BRIGHTNESS );
+  FastLED.clear();
 
   nunchuck1.begin();
   if (nunchuck1.type == Unknown) {
     nunchuck1.type = NUNCHUCK;
   }
 
-  ledMillis = chuckMillis = 0;
+  ledMillis = controllerMillis = 0;
 }
 
 void loop() {
   unsigned long curMillis = millis();
-  if (curMillis > ledMillis + 10) {
+  
+  // LED loop
+  if (curMillis > ledMillis + 1000) {
     ledMillis = curMillis;
-    leds[0] = CRGB::Red;
-    leds[1] = CRGB::Red;
-    leds[2] = CRGB::Red;
-    leds[3] = CRGB::Red;
-    leds[4] = CRGB::Black;
-    leds[5] = CRGB::Blue;
-    leds[6] = CRGB::Blue;
-    leds[7] = CRGB::Yellow;
-    leds[8] = CRGB::Blue;
-    leds[9] = CRGB::Blue;
-    leds[10] = CRGB::Blue;
-    leds[11] = CRGB::Black;
+
+    //health tracker
+    leds[0] = Red;
+    leds[1] = Red;
+    leds[2] = Red;
+    leds[3] = Green;
+    for (int i = 4; i > -1; i--) {
+      if (healthTracker - 1 >= i) leds[i] = Red;
+      else leds[i] = Green;
+    }
+
+    // divider LEDs
+    leds[4] = leds[11] = Black;
+
+    // sky
+    for (int j = 5; j < 11; j++) {
+      if (sunMoonPhase < 6) leds[j] = Blue; //day
+      else leds[j] = LightBlue; //night
+    }
+
+    // day/night cycle
+    if (sunMoonPhase < 6) { //day
+      leds[sunMoonPhase + 5] = Yellow;
+    } else if (sunMoonPhase > 5 && sunMoonPhase < 12) { //night
+      leds[sunMoonPhase - 1] = White;
+    }
+
+    if (sunMoonPhase >= 11) {
+      sunMoonPhase = 0;
+    } else {
+      sunMoonPhase++;
+    }
+
     FastLED.show();
   }
-  if (curMillis > chuckMillis + 250) {
-    chuckMillis = curMillis;
+
+  // controller loop
+  if (curMillis > controllerMillis + _controllerUpdateRate) {
+    controllerMillis = curMillis;
     nunchuck1.readData();    // Read inputs and update maps
     nunchuck1.printInputs(); // Print all inputs
   }

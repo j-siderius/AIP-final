c6a5c5c40a8c94b83b4fa36a6f6612eec41cf19a
TFvrDev <nick.rius@gmail.com>
2022-01-21 14:55:47 +0100

    added event listening functions implemented input class

--- main.py
+++ main.py
@@ -4,17 +4,20 @@ from Field import Field
 from Player import Player
 from Serial import Serial
 import time
+from Input import Input
 
 
 class program:
 
     def __init__(self):
         # self.screen = Screen(584, 592, self.loop, title="Test Terrain")
-        self.screen = Screen(0, 0, self.loop, title="Test Terrain", mouse_pressed_func=self.mouse_pressed)
+        self.screen = Screen(0, 0, self.loop, title="Test Terrain", mouse_pressed_func=self.mouse_pressed, mouse_moved_func=self.mouse_moved)
 
         self.field = Field(self.screen, hex_width=4 * 11, field_size=(self.screen.get_size()))
         self.player = Player(self.screen, field_size=(self.screen.get_size()), field=self.field)  # need to implement hex calculation in class
         self.serial = Serial('COM14')  # COM14 is PC, /dev/cu.wchusbserial1410 is MAC
+        self.input = Input(self.player)
+        self.screen.set_input_class(self.input)
 
         self.screen.start()
 
@@ -30,8 +33,11 @@ class program:
         self.screen.text_color(0)
         self.screen.text(5, 5, f"{self.screen.get_frameRate():.2f}", False)
 
+    def mouse_moved(self):
+        self.input.process_mouse_movement(self.screen.get_mouse_pos())
+
     def mouse_pressed(self, button):
-        self.player.mouse_pressed(self.screen.get_mouse_pos(), button)
+        self.input.process_mouse_button(button)
 
 
 if __name__ == '__main__':

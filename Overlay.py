33c1175f1cee838924ecf2f566f6dba3c854846f
Frank <frankbsmn@gmail.com>
2022-01-25 16:00:14 +0100

    fixed and commented overlay scaling and positioning

--- Overlay.py
+++ Overlay.py
@@ -1,43 +1,40 @@
 import pygame
 
+from Screen import Screen
+
 
 class Overlay:
-    def __init__(self, screen, controller):
-        self.screen = screen
+    def __init__(self, screen: Screen, controller):
+        self.screen: Screen = screen
         self.screen_width, self.screen_height = self.screen.get_size()[0], self.screen.get_size()[1]
-        img_width = int(self.screen_width * 0.5)  # based on images of 1920ximg_specific
-        self.img_pos = (int(self.screen_width/4), int(self.screen_height/5))
 
         self.start = True
         self.end = False
+
+        # the place relative place on the screen the continue text is
+        self.continue_height_scale = 3.34
 
         self.score: int = -1
         self.timescale, self.game_duration = controller.get_timescale()
 
         # initializing all screen images and resizing them to fit screen
         self.won_screen = pygame.image.load("Data/Images/won.png").convert_alpha()
-        img_height = int(self.won_screen.get_size()[1] * 0.6)
-        self.win_screen = pygame.transform.scale(self.won_screen, (img_width, img_height))
+        self.won_screen, self.won_rect = self.scale_image(self.won_screen)
 
         self.lost_screen = pygame.image.load("Data/Images/lost.png").convert_alpha()
-        img_height = int(self.lost_screen.get_size()[1] * 0.6)
-        self.lost_screen = pygame.transform.scale(self.lost_screen, (img_width, img_height))
+        self.lost_screen, self.lost_rect = self.scale_image(self.lost_screen)
 
         self.tut0 = pygame.image.load("Data/Images/tutorial0.png").convert_alpha()
-        img_height = int(self.tut0.get_size()[1] * 0.6)
-        self.tut0 = pygame.transform.scale(self.tut0, (img_width, img_height))
+        self.tut0, self.tut0_rect = self.scale_image(self.tut0)
 
         self.tut1 = pygame.image.load("Data/Images/tutorial1.png").convert_alpha()
-        img_height = int(self.tut1.get_size()[1] * 0.6)
-        self.tut1 = pygame.transform.scale(self.tut1, (img_width, img_height))
+        self.tut1, self.tut1_rect = self.scale_image(self.tut1)
 
         self.tut2 = pygame.image.load("Data/Images/tutorial2.png").convert_alpha()
-        img_height = int(self.tut2.get_size()[1] * 0.6)
-        self.tut2 = pygame.transform.scale(self.tut2, (img_width, img_height))
+        self.tut2, self.tut2_rect = self.scale_image(self.tut2)
 
         self.tut3 = pygame.image.load("Data/Images/tutorial3.png").convert_alpha()
-        img_height = int(self.tut3.get_size()[1] * 0.6)
-        self.tut3 = pygame.transform.scale(self.tut3, (img_width, img_height))
+        self.tut3, self.tut3_rect = self.scale_image(self.tut3)
 
         self.tutorial_slide = 0
 
@@ -58,38 +55,48 @@ class Overlay:
             self.end_screen()
 
     def start_screen(self):
+        """
+        Shows the current tutorial overlay
+        """
         self.background.fill((150, 150, 150))
         self.background.set_alpha(75)
         self.screen.get_screen().blit(self.background, (0, 0))
 
         if self.tutorial_slide == 0:
-            self.screen.get_screen().blit(self.tut0, (self.img_pos[0], self.img_pos[1]))
+            # show the tutorial image on the screen
+            self.screen.get_screen().blit(self.tut0, self.tut0_rect.topleft)
 
+            # show the continue text
             self.screen.set_font(self.small_font)
-            self.screen.text(self.screen_width / 2, (self.screen_height / 5) * 3.2, "Press [left] or [z] to continue")
+            self.screen.text(self.screen_width / 2, (self.screen_height / 5) * self.continue_height_scale, "Press [left] or [z] to continue")
         elif self.tutorial_slide == 1:
-            self.screen.get_screen().blit(self.tut1, (self.img_pos[0], self.img_pos[1]))
+            # show the tutorial image on the screen
+            self.screen.get_screen().blit(self.tut1, self.tut1_rect.topleft)
 
+            # show the continue text
             self.screen.set_font(self.small_font)
-            self.screen.text(self.screen_width / 2, (self.screen_height / 5) * 3.2, "Press [left] or [z] to continue")
+            self.screen.text(self.screen_width / 2, (self.screen_height / 5) * self.continue_height_scale, "Press [left] or [z] to continue")
         elif self.tutorial_slide == 2:
-            self.screen.get_screen().blit(self.tut2, (self.img_pos[0], self.img_pos[1]))
+            self.screen.get_screen().blit(self.tut2, self.tut2_rect.topleft)
 
             self.screen.set_font(self.small_font)
-            self.screen.text(self.screen_width / 2, (self.screen_height / 5) * 3.2, "Press [left] or [z] to continue")
+            self.screen.text(self.screen_width / 2, (self.screen_height / 5) * self.continue_height_scale, "Press [left] or [z] to continue")
         else:
-            self.screen.get_screen().blit(self.tut3, (self.img_pos[0], self.img_pos[1]))
+            self.screen.get_screen().blit(self.tut3, self.tut3_rect.topleft)
 
             self.screen.set_font(self.small_font)
-            self.screen.text(self.screen_width / 2, (self.screen_height / 5) * 3.2, "Press [left] or [z] to start")
+            self.screen.text(self.screen_width / 2, (self.screen_height / 5) * self.continue_height_scale, "Press [left] or [z] to start")
 
     def end_screen(self):
+        """
+        Show the end screen overlay, depending on the win/lose state
+        """
         self.screen.get_screen().blit(self.background, (0, 0))
 
-        if self.score < self.game_duration:
-            self.screen.get_screen().blit(self.lost_screen, (self.img_pos[0], self.img_pos[1]))
-        else:
-            self.screen.get_screen().blit(self.won_screen, (self.img_pos[0], self.img_pos[1]))
+        if self.score < self.game_duration:  # if you lost it shows you the lose screen overlay
+            self.screen.get_screen().blit(self.lost_screen, self.lost_rect.topleft)
+        else:  # show the win screen overlay
+            self.screen.get_screen().blit(self.won_screen, self.won_rect.topleft)
 
         self.screen.set_font(self.bold_font)
         self.screen.text(self.screen_width/2, (self.screen_height/5) * 2.8, f"You survived {self.score} hours")
@@ -97,10 +104,17 @@ class Overlay:
         self.screen.text(self.screen_width / 2, (self.screen_height / 5) * 3, "(Press [left] or [z] to quit)")
 
     def update_end(self, score: int = -1):
+        """
+        Set the end, win/lose state
+        :param score: The amount of 'hours' you survived
+        """
         self.end = True
         self.score = score
 
     def update_start(self):
+        """
+        show the next tutorial overlay or closes it
+        """
         if self.tutorial_slide < 3:
             self.tutorial_slide += 1
         else:
@@ -108,3 +122,22 @@ class Overlay:
             self.background.set_alpha(0)
             self.screen.get_screen().blit(self.background, (0, 0))
 
+    def scale_image(self, image: pygame.Surface) -> (pygame.Surface, pygame.Rect):
+        """
+        Resizes the images to fit the screen while keeping the aspect ratio the intact.
+        And makes a rectangle of the image that is centered on the screen.
+        :param image: The image that has to be resized
+        :return: The resized image and the centered rectangle
+        """
+        width, height = image.get_size()
+        screen_width, screen_height = self.screen.get_size()
+        aspect_ratio = width / height
+
+        img_width = int(self.screen_width * 0.5)
+        img_height = int(img_width / aspect_ratio)
+        img = pygame.transform.scale(image, (img_width, img_height))
+
+        # get the rectangle bounding box and move it to be centered on the screen
+        rect: pygame.Rect = img.get_rect()
+        rect.center = (screen_width/2, screen_height/2)
+        return img, rect
